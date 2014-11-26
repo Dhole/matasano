a6da63127bcb5a54a256a98b1fd0fc14cc435b2d
Dhole <bankukur@gmail.com>
2014-11-26 23:25:25 +0100

    update

--- go/s1c6.go
+++ go/s1c6.go
@@ -22,8 +22,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//key := make([]byte, 1)
-
 	min_dist := 8.0
 	min_key_size := 2
 	for i := 2; i < 40; i++ {
@@ -54,41 +52,4 @@ func main() {
 	}
 	fmt.Println("key:", string(key))
 	fmt.Println("text:\n", string(set1.XorKey(input, key)))
-	//fmt.Println(min_key_size)
-	/*
-		for key_len := 2; key_len < 40; key_len++ {
-
-
-
-
-		min_min_score := 10.0
-		min_min_key := make([]byte, 1)
-		var min_min_input []byte
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			input, _ := hex.DecodeString(scanner.Text())
-
-			min_score := 10.0
-			min_key := make([]byte, 1)
-
-			for i := 0x00; i < 0x100; i++ {
-				key[0] = byte(i)
-				xored := set1.XorKey(input, key)
-				hist := set1.ComputeHist(xored)
-				score := set1.ComputeScore(set1.Eng_freq, hist)
-				if score < min_score {
-					min_score = score
-					copy(min_key, key)
-				}
-			}
-			if min_score < min_min_score {
-				min_min_score = min_score
-				min_min_key = min_key
-				min_min_input = input
-			}
-		}
-
-		fmt.Println(string(set1.XorKey(min_min_input, min_min_key)))
-	*/
 }
