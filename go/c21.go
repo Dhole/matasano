9dea7373c107b3956758b134e397f91745ae4381
Dhole <bankukur@gmail.com>
2015-01-23 23:33:12 +0100

    Finished challenge 22

--- go/c21.go
+++ go/c21.go
@@ -1,50 +1,12 @@
 package main
 
 import (
+	"./mt19937"
 	"fmt"
 )
 
-type MT19937 struct {
-	index int
-	MT    []uint32
-}
-
-func (m *MT19937) Init(seed uint32) {
-	m.MT = make([]uint32, 624)
-	m.index = 0
-	m.MT[0] = seed
-	for i := uint32(1); i < 624; i++ {
-		m.MT[i] = (0x6c078965*(m.MT[i-1]^(m.MT[i-1]>>30)) + i)
-	}
-}
-
-func (m *MT19937) ExtractNumber() uint32 {
-	if m.index == 0 {
-		m.generateNumbers()
-	}
-
-	y := m.MT[m.index]
-	y = y ^ (y >> 11)
-	y = y ^ ((y << 7) & 0x9d2c5680)
-	y = y ^ ((y << 15) & 0xefc60000)
-	y = y ^ (y >> 18)
-
-	m.index = (m.index + 1) % 264
-	return y
-}
-
-func (m *MT19937) generateNumbers() {
-	for i := 0; i < 624; i++ {
-		y := (m.MT[i] & 0x80000000) + (m.MT[(i+1)%624] & 0x7fffffff)
-		m.MT[i] = m.MT[(i+397)%624] ^ (y >> 1)
-		if (y % 2) != 0 {
-			m.MT[i] = m.MT[i] ^ 0x9908b0df
-		}
-	}
-}
-
 func main() {
-	var m MT19937
+	m := mt19937.New()
 	m.Init(0x1571)
 
 	for i := 0; i < 10; i++ {
---
+++ go/c22.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"./mt19937"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var fake_time uint32
+
+func waitFakeTime(t uint32) {
+	fake_time += t
+}
+
+func getFakeTime() uint32 {
+	return fake_time
+}
+
+func initFakeTime(t uint32) {
+	fake_time = t
+}
+
+func genRandom() (val uint32) {
+	m := mt19937.New()
+
+	waitFakeTime(uint32(rand.Int31n(10000-40) + 40))
+	fmt.Println("Real seed:", getFakeTime())
+	m.Init(getFakeTime())
+	val = m.ExtractNumber()
+	waitFakeTime(uint32(rand.Int31n(10000-40) + 40))
+	return val
+}
+
+func crackSeed(val uint32) (seed uint32) {
+	t := getFakeTime()
+	fmt.Println("Current time:", t)
+	m := mt19937.New()
+	for {
+		//fmt.Println("Trying for ", t)
+		m.Init(t)
+		if m.ExtractNumber() == val {
+			return t
+		}
+		t--
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	initFakeTime(uint32(time.Now().UnixNano()))
+	val := genRandom()
+	fmt.Println("Random value:", val)
+	seed := crackSeed(val)
+	fmt.Println("Seed found:", seed)
+	m := mt19937.New()
+	m.Init(seed)
+	fmt.Println("Test -> seed:", seed, "generates:", m.ExtractNumber())
+}
